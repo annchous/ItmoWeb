22d16493c2eb80021eea3b5a48565253a2877aa0
annchous <annchooousss@gmail.com>
2021-10-20 15:41:03 +0300

    refactor: move script to separate file

---
+++ src/js/page-onload.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    var footer = document.getElementsByClassName('footer-bar')[0];
+    footer.innerHTML += '<p class="footer-bar">Page load time is ' + loadTime + ' seconds</p>';
+
+    var url = window.location.href;
+    console.log("URL: " + url)
+
+    $(".menu-list a").each(function() {
+        console.log(this.href);
+        if (url == this.href) {
+            $(this).addClass('menu-list-active');
+        }
+    });
+}
