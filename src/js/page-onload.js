e2e40b8f307b543b769a08ade945ae6c70a6ca66
annchous <annchooousss@gmail.com>
2021-12-08 00:46:21 +0300

    fix: fix text

--- src/js/page-onload.js
+++ src/js/page-onload.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
     var footer = document.getElementsByClassName('footer-bar')[0];
-    footer.innerHTML += '<p class="footer-bar">Page load time is ' + loadTime + ' seconds</p>';
+    footer.innerHTML += '<p class="footer-bar">Page load time is ' + loadTime + ' ms</p>';
 
     var url = window.location.href;
     console.log("URL: " + url)
